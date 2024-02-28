6d9e6d11ca65c00d12964c801f8cd42d6be47711
Max <7enidomaks@gmail.com>
2024-02-28 12:04:36 +0300

    Изменения для PEP-8

--- task2.py
+++ task2.py
@@ -1,27 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-arr = np.genfromtxt('data2.csv',delimiter='',dtype=None,encoding='utf-8')
+arr = np.genfromtxt('data2.csv', delimiter='', dtype=None, encoding='utf-8')
 arr1 = np.vstack(arr)
 arr2 = []
 data = []
 for i in range(len(arr1)):
-    arr2.append(str(arr1[i]).replace("'",'').split(";"))
+    arr2.append(str(arr1[i]).replace("'", '').split(";"))
 for i in range(1, len(arr2)):
-    arr2[i][0] = arr2[i][0].replace('[','')
+    arr2[i][0] = arr2[i][0].replace('[', '')
     if arr2[i][0] == '':
         arr2[i][0] = 0.0
     data.append(float(arr2[i][0]))
 
-plt.figure(figsize = (10, 5))
-plt.hist(data, bins = 20)
+plt.figure(figsize=(10, 5))
+plt.hist(data, bins=20)
 plt.title('Гистограмма данных')
 plt.xlabel('Значения')
 plt.ylabel('Частота')
 plt.show()
 
-plt.figure(figsize = (10, 5))
-plt.hist(data, bins = 20, density=True)
+plt.figure(figsize=(10, 5))
+plt.hist(data, bins=20, density=True)
 plt.title('Нормализованная гистограмма данных')
 plt.xlabel('Значения')
 plt.ylabel('Плотность')
@@ -29,7 +29,3 @@ plt.show()
 
 std_dev = np.std(data)
 print(f'Среднеквадратичное отклонение данных: {std_dev}')
-
-
-
-
--- task3.py
+++ task3.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(-2*np.pi,2*np.pi)
+x = np.linspace(-2*np.pi, 2*np.pi)
 y = np.sin(x)*np.cos(x)
 z = np.sin(x)*np.cos(x)
 fig = plt.figure()
-ax = fig.add_subplot(111,projection="3d")
-ax.plot(x,y,z)
+ax = fig.add_subplot(111, projection="3d")
+ax.plot(x, y, z)
 ax.set_xlabel = "X"
 ax.set_ylabel = "Y"
 ax.set_zlabel = "Z"
